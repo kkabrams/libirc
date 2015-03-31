693a26b356905deef984f2f452c48346c4901395
epochqwert <epoch@hacking.allowed.org>
2015-03-31 13:25:06 -0500

    added user struct to irc.h

--- irc.h
+++ irc.h
@@ -1,4 +1,11 @@
+struct user {
+ char *nick;
+ char *user;
+ char *host;
+};
+
 int runit(int fd,void (*line_handler)(),void (*extra_handler)()) ;
+int runem(int *fd,void (*line_handler)(),void (*extra_handler)()) ;
 int ircConnect(char *serv,char *port,char *nick,char *user) ;
 int serverConnect(char *serv,char *port) ;
 char **line_cutter(int fd,char *line,struct user *user) ;
