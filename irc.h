61afdc45958819e693fce9e8646e57729fca0359
epoch <epoch@hack.thebackupbox.net>
2019-06-08 11:04:54 +0000

    added irc_handler to header so segfault can use it

--- irc.h
+++ irc.h
@@ -9,3 +9,4 @@ int runem(int *fd,void (*line_handler)(),void (*extra_handler)()) ;
 int ircConnect(char *serv,char *port,char *nick,char *user) ;
 int serverConnect(char *serv,char *port) ;
 char **line_cutter(int fd,char *line,struct user *user) ;
+void irc_handler(struct shit *me,char *line);
